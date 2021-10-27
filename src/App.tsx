03832c6c1717b5e642d6bd8338bc32a7b2532f44
Anton Levshunov <alevshunov@gmail.com>
2021-10-27 14:00:21 +0300

    poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="ui primary button">
+        Save
+      </button>
+      <button className="ui button">
+        Discard
+      </button>
+
+        <div className="ui labeled button" tabIndex={0}>
+            <div className="ui red button">
+                <i className="heart icon"></i> Like
+            </div>
+            <a className="ui basic red left pointing label">
+                1,048
+            </a>
+        </div>
+        <div className="ui labeled button" tabIndex={0}>
+            <div className="ui basic blue button">
+                <i className="fork icon"></i> Forks
+            </div>
+            <a className="ui basic left pointing blue label">
+                1,048
+            </a>
+        </div>
+    </>
   );
 }
 
